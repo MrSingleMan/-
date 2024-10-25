b5110bf43051fc4ce7aa4d3d1d8c7f4a439ea21a
Alexander <n00mer00k@gmail.com>
2024-10-25 20:15:39 +0400

    New Function

--- Animal/src/Main.java
+++ Animal/src/Main.java
@@ -2,24 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        AnimalRegistry registry = new AnimalRegistry();
-        Scanner scanner = new Scanner(System.in);
 
-        // Пример добавления животных
-        Dogs dog = new Dogs("Buddy", "2020-01-01");
-        Cats cat = new Cats("Whiskers", "2019-05-12");
-        registry.addAnimal(dog);
-        registry.addAnimal(cat);
-
-        // Добавляем команды
-        registry.trainAnimal(dog, "Сидеть");
-        registry.trainAnimal(cat, "Лежать");
-
-        // Выводим команды
-        registry.showCommands(dog);
-        registry.showCommands(cat);
-
-        // Сортируем и выводим животных по дате рождения
-        registry.listAnimalsByBirthDate();
     }
 }
